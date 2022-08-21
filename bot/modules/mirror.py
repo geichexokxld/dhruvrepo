3e3eb85310c544c2342bc852f0dfa66f42cc06bc
Dhruv045 <106961058+Dhruv045@users.noreply.github.com>
2022-08-22 02:42:08 +0530

    Fixup!

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -245,7 +245,7 @@ class MirrorListener:
             msg += f'\n<b>It Tooks:</b> {get_readable_time(time() - self.message.date.timestamp())}'
             msg += f'\n\n<b>Thank You For using {TITLE_NAME}! Keep Supporting & Keep Loving!</b>'
             if not files:
-                sendMessage(msg, self.bot, self.message)
+                sendMarkup(msg, self.bot, self.message)
             else:
                 fmsg = '\n<b>Your Files Are:</b>\n'
                 for index, (link, name) in enumerate(files.items(), start=1):
@@ -255,7 +255,7 @@ class MirrorListener:
                         sleep(1)
                         fmsg = ''
                 if fmsg != '':
-                    sendMessage(msg + fmsg, self.bot, self.message)
+                    sendMarkup(msg + fmsg, self.bot, self.message)
                 if LEECH_LOG:
                     try:
                         for chatid in LEECH_LOG:
